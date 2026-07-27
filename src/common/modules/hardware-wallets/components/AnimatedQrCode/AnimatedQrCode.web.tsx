850eaa3cec72b61795f5139fdd7042596b61647e
Borislav Itskov <borislav@devlabs.bg>
2026-07-27 19:14:25 +0900

    improve qr code readibility as it's not good enough for keycard

--- src/common/modules/hardware-wallets/components/AnimatedQrCode/AnimatedQrCode.web.tsx
+++ src/common/modules/hardware-wallets/components/AnimatedQrCode/AnimatedQrCode.web.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { View } from 'react-native'
 
 import { AnimatedQRCode } from '@keystonehq/animated-qr'
+import spacings, { SPACING_SM } from '@common/styles/spacings'
+import flexbox from '@common/styles/utils/flexbox'
 
 import { AnimatedQrCodeProps } from './AnimatedQrCode'
 
 const DEFAULT_SIZE = 300
 const DEFAULT_INTERVAL = 300
+const MAX_FRAGMENT_LENGTH = 200
+const QR_BACKGROUND_COLOR = '#fff'
 
 // Web renders the animated QR through @keystonehq/animated-qr, which relies on
 // qrcode.react (browser canvas) and is therefore web-only. The native variant
@@ -15,6 +20,25 @@ const AnimatedQrCode = ({
   cbor,
   size = DEFAULT_SIZE,
   interval = DEFAULT_INTERVAL
-}: AnimatedQrCodeProps) => <AnimatedQRCode options={{ size, interval }} type={type} cbor={cbor} />
+}: AnimatedQrCodeProps) => (
+  <View
+    style={[
+      flexbox.center,
+      spacings.pSm,
+      { width: size, height: size, backgroundColor: QR_BACKGROUND_COLOR }
+    ]}
+  >
+    <AnimatedQRCode
+      options={{
+        capacity: MAX_FRAGMENT_LENGTH,
+        interval,
+        // AnimatedQRCode already adds 5px of white space on every side.
+        size: size - SPACING_SM * 2
+      }}
+      type={type}
+      cbor={cbor}
+    />
+  </View>
+)
 
 export default React.memo(AnimatedQrCode)
